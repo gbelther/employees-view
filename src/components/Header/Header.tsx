9f0930077a270057137b0645d3d4961339b642bf
gbelther <belthergabriel@gmail.com>
2021-10-28 22:22:38 -0300

    feature: criação do componente Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <header id="header" className="header">
+      <div className="header__logo-wrapper">
+        <img src="/images/bemobile-logo.svg" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
