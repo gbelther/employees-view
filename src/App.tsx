4d9321c727838489396ac81e5054a15f8cfd7abf
gbelther <belthergabriel@gmail.com>
2021-10-30 13:16:33 -0300

    feature: montagem da página utilizando os componentes e estilização da table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
+import { AiOutlineSearch } from "react-icons/ai";
+
+import { Header, Heading, InputText } from "./components";
+
+import "./styles/global.scss";
+import "./styles.scss";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <main id="main-content">
+        <section className="main-content__title-input-wrapper">
+          <Heading>Funcionários</Heading>
+          <InputText
+            type="text"
+            placeholder="Pesquisar"
+            icon={<AiOutlineSearch />}
+          />
+        </section>
+        <section className="main-content__table-wrapper">
+          <table className="main-content__table-wrapper--table">
+            <thead>
+              <tr>
+                <th>FOTO</th>
+                <th>NOME</th>
+                <th>CARGO</th>
+                <th>DATA DE ADMISSÃO</th>
+                <th>TELEFONE</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <img
+                    src="/images/avatar_generic.svg"
+                    alt="Avatar do usuário"
+                    width={32}
+                    height={32}
+                  />
+                </td>
+                <td>Rodrigo Mota</td>
+                <td>Front-end</td>
+                <td>00/00/0000</td>
+                <td>+55 (55) 55555-5555</td>
+              </tr>
+              <tr>
+                <td>
+                  <img
+                    src="/images/avatar_generic.svg"
+                    alt="Avatar do usuário"
+                    width={32}
+                    height={32}
+                  />
+                </td>
+                <td>Rodrigo Mota</td>
+                <td>Front-end</td>
+                <td>00/00/0000</td>
+                <td>+55 (55) 55555-5555</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+    </>
   );
 };
 
