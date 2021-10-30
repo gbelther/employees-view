b817055c791d9b6eb2790a956f100a84ed14f961
gbelther <belthergabriel@gmail.com>
2021-10-30 14:22:51 -0300

    feature: formatação do período de admissão

--- src/hooks/useFormatEmployes.ts
+++ src/hooks/useFormatEmployes.ts
@@ -5,11 +5,13 @@ import { formatPhoneNumber } from "../utils/numberFormats";
 export const useFormatEmployes = (data: IGetEmployesData): IEmployes => {
   const { id, name, job, admission_date, phone, image } = data;
 
+  const arrayDate = admission_date.slice(0, 10).split("-");
+
   return {
     id,
     name,
     job,
-    admissionDate: admission_date,
+    admissionDate: `${arrayDate[2]}/${arrayDate[1]}/${arrayDate[0]}`,
     phone: formatPhoneNumber(phone),
     image,
   };
